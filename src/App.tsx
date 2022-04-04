f7f8133b20feacb71c9d3ce7c17f0f70ed51f477
Dan Christopher Saldariega <dcsaldariega@dnamicro.com>
2022-04-05 07:45:57 +0800

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Register from './components/Register';
 
 class App extends React.Component {
   constructor(props:any) {
@@ -14,10 +15,35 @@ class App extends React.Component {
 
     }
   }
+
+  handleChangeFullName(e:any){
+    this.setState({
+      fullName:e.target.value
+    })
+  }
+  handleChangeUsername(e:any){
+    this.setState({
+      username:e.target.value
+    })
+  }
+  handleChangeEmail(e:any){
+    this.setState({
+      email:e.target.value
+    })
+  }
+  handleChangePassword(e:any){
+    this.setState({
+      password:e.target.value
+    })
+  }
+
+
+
   render() {
     return (
       <div>
         <Login user={this.state}/>
+        <Register user={this.state} handleChange={[this.handleChangeFullName, this.handleChangeUsername, this.handleChangeEmail, this.handleChangePassword]}/>
       </div>
     );
   }
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
-function Register() {
+function Register(props: any) {
+  const { fullName, username, email, password } = props.user
+  
   return (
-    <div>Register</div>
+    <div>
+      <div>Register</div>
+      <div>
+        <input type='text' 
+        placeholder='Full Name' 
+        onChange={()=> {}}
+        value={fullName}/>
+        <input type='text' 
+        placeholder='Username' 
+        onChange={()=> {}}
+        value={username}/>
+        <input type='text' 
+        placeholder='Email' 
+        onChange={()=> {}}
+        value={email}/>
+        <input type='password' 
+        placeholder='Password' 
+        onChange={()=> {}}
+        value={password}/></div>
+        <button>Register</button>
+    </div>
   )
 }
 
